519766e0f699fe394c73042956915e405533c08b
dilaybk <dilay.bayraktaroglu@hva.nl>
2023-05-22 11:13:40 +0200

    1

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,15 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0,		//sound 0 - dus geen geluid
+	9,		// sound 1 (EEEEEE)
+	12,		// sound 2 (Siren)
+	15,		// sound 3 (kuzzzzz-... 1)
+	16.5,	// sound 4 (...-DUUUNNN 1)
+	24,		// sound 5 (kuzzzzz-... 2)
+	25.8,	// sound 6 (...-DUUUNNN 2)
+	31,		// sound 7 (kuzzzzz-... louder 3)
+	32,		// sound 8 (...-DUUUNNN louder 3)
+	34,		// sound 9 (slowly higher noise)
+	86		// sound 10
 ];
